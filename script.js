1b87d0bb914efa3602d0f1533cadb77f865e607a
Juan-Kmkz <juandanilo777@gmail.com>
2024-12-15 14:02:21 -0300

    Projeto semi-finalizado, faltando ajustes finos

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const actionButton = document.getElementById("draw")
 
 
 function pressed (){
-    const min = document.getElementById("number-one").value
-    const max = document.getElementById("number-two").value
+    const min = Math.ceil(document.getElementById("number-one").value)
+    const max = Math.floor(document.getElementById("number-two").value)
     const displayResult = document.getElementById("random-result")
     
-    const result = Math.floor(Math.random() * (max - min + 1) + min); 
+    const result = Math.floor(Math.random()*(max - min + 1) + min); 
 
     displayResult.innerText = result
 }
