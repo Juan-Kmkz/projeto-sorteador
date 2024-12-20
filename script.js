820ecc485f6e8f1e0f33eeb4b65fb53062ec03d6
Juan-Kmkz <juandanilo777@gmail.com>
2024-12-20 18:19:22 -0300

    Formatacao do codigo

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 const actionButton = document.getElementById("draw")
 
-
-function pressed (){
-
+function pressed() {
     const min = Math.ceil(document.getElementById("number-one").value)
     const max = Math.floor(document.getElementById("number-two").value)
     const displayResult = document.getElementById("random-result")
 
-    if(min >= max){
+    if (min >= max) {
         alert("O primeiro número deve ser MENOR que o segundo!")
     }
-        else{
-            const result = Math.floor(Math.random()*(max - min + 1) + min); 
-            displayResult.innerText = result
-        }
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1) + min);
+        displayResult.innerText = result
+    }
 }
-
-actionButton.addEventListener("click",pressed)
+actionButton.addEventListener("click", pressed)
 
