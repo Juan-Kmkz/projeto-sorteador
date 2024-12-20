57c0947989f75cc26bb247e6b801b8c0608748d1
Juan-Kmkz <juandanilo777@gmail.com>
2024-12-20 18:09:42 -0300

    Incrementos realizados no projeto.

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ const actionButton = document.getElementById("draw")
 
 
 function pressed (){
+
     const min = Math.ceil(document.getElementById("number-one").value)
     const max = Math.floor(document.getElementById("number-two").value)
     const displayResult = document.getElementById("random-result")
-    
-    const result = Math.floor(Math.random()*(max - min + 1) + min); 
 
-    displayResult.innerText = result
+    if(min >= max){
+        alert("O primeiro número deve ser MENOR que o segundo!")
+    }
+        else{
+            const result = Math.floor(Math.random()*(max - min + 1) + min); 
+            displayResult.innerText = result
+        }
 }
 
-
 actionButton.addEventListener("click",pressed)
+
